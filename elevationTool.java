adc5bd93f97f76ad338ae280c616e3e42a3d6686
Austin <16212918+XxaustinschulzxX@users.noreply.github.com>
2022-02-19 22:35:13 -0500

    These work, awaiting optimization and extendability

--- elevationTool.java
+++ elevationTool.java
@@ -14,6 +14,7 @@ static Scanner sc;
 static gpsCoordinate[] coordinates = new gpsCoordinate[512];
 static double[] latitudes = new double[512];
 static double[] longitudes = new double[512];
+static double[] elevations = new double[512];
 
     public static void main(String[] args) throws IOException {
         //Taking user input
@@ -45,16 +46,6 @@ static double[] longitudes = new double[512];
             longitudes[256 - i] = longitude - i * 0.005 / 256;
             latitudes[256 - i] = latitude - i * 0.005 / 256;
         }
-
-        /*Filling array of gpsCoordinate objects, printing for diagnostics
-        for (int i = 0; i < 512; i++) {
-            coordinates[i] = new gpsCoordinate(latitudes[i], longitudes[i]);
-            System.out.println("latitude # " + i + " " + latitudes[i]);
-            System.out.println("longitude # " + i + " " + longitudes[i]);
-        }
-        */
-
-
         //Single point google cloud API call
         //URL url = new URL("https://maps.googleapis.com/maps/api/elevation/json?locations=" + latitude + "%2C" + longitude + "&key=" + apiKey);
 
@@ -72,18 +63,61 @@ static double[] longitudes = new double[512];
         URL url = new URL("https://maps.googleapis.com/maps/api/elevation/json?locations=" + coordinateString + "&key=" + apiKey);
 
         //Processing API data returned
-        String[] stringData = new String[512];
-        double[] doubleData = new double[512];
+        String[] stringData;
         String json = new Scanner(url.openStream(), "UTF-8").useDelimiter("\\A").next();
         JsonParser jsonParser = new JsonParser();
-         data = jsonParser.parse(json).getAsJsonArray();
-        System.out.println(data);
+        String data = jsonParser.parse(json).toString();
 
-        for (int i = 0; i < 512; i++) {
-            doubleData[i] = Double.parseDouble(stringData[i]);
+        stringData = data.split("elevation\":");
+        String[] stringDataFixed = new String[512];
+        for (int i = 1; i < 513; i++) {
+            stringDataFixed[i - 1] = stringData[i];
         }
 
-        int largest;
-    }
+        int counter = 0;
+        char[] temp;
+        for (String s : stringDataFixed) {
+            temp = s.toCharArray();
+            for (int i = 0; i < temp.length; i++) {
+                if (temp[i] == ',') {
+                    s = new String(temp);
+                    s = s.substring(0, i);
+                    stringDataFixed[counter] = s;
+                    break;
+                }
+            }
+            counter++;
+        }
 
+        for (int i = 0; i < 512; i++) {
+            elevations[i] = Double.parseDouble(stringDataFixed[i]);
+        }
+
+        //Filling array of gpsCoordinate objects, printing for diagnostics
+        for (int i = 0; i < 512; i++) {
+            coordinates[i] = new gpsCoordinate(latitudes[i], longitudes[i], elevations[i]);
+        }
+
+        //finds maximum elevation
+        double largest = -11035; //marianas trench
+        double smallest = 8849; //mount everest
+        int indexL = -1;
+        int indexS = -1;
+        for (int i = 0; i < 512; i++) {
+            if (elevations[i] > largest) {
+                largest = elevations[i];
+                indexL = i;
+            } else if (elevations[i] < smallest) {
+                smallest = elevations[i];
+                indexS = i;
+            }
+        }
+
+        if (largest > -11035) {
+            System.out.println("\nThe highest elevation within the region is: " + largest + " meters at location " + latitudes[indexL] + ", " + longitudes[indexL]);
+        }
+        if (smallest < 8849) {
+            System.out.println("The lowest elevation within the region is: " + smallest + " meters at location " + latitudes[indexS] + ", " + longitudes[indexS]);
+        }
+    }
 }
--- gpsCoordinate.java
+++ gpsCoordinate.java
@@ -1,9 +1,11 @@
 public class gpsCoordinate {
     double latitude;
     double longitude;
+    double elevation;
 
-    public gpsCoordinate(double latitude, double longitude) {
+    public gpsCoordinate(double latitude, double longitude, double elevation) {
         this.latitude = latitude;
         this.longitude = longitude;
+        this.elevation = elevation;
     }
 }
