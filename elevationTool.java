f5aafe05e656a2115c387ba2b45513fe066b72e8
Austin <16212918+XxaustinschulzxX@users.noreply.github.com>
2022-02-19 23:32:53 -0500

    Functional, needs some polish

--- elevationTool.java
+++ elevationTool.java
@@ -6,48 +6,56 @@ import java.net.URL;
 import java.util.Scanner;
 
 public class elevationTool {
-static double squareSide = 0.01;
+static double searchSizeLat;
+static double searchSizeLong;
 static double latitude;
 static double longitude;
 static String apiKey = "AIzaSyDvwxHWtxixS7JVMGzRGGoJZu0JtT-uYxc";
 static Scanner sc;
-static gpsCoordinate[] coordinates = new gpsCoordinate[512];
 static double[] latitudes = new double[512];
 static double[] longitudes = new double[512];
 static double[] elevations = new double[512];
 
     public static void main(String[] args) throws IOException {
+
+        try {
+            searchSizeLat = (Double.parseDouble(args[1]))/2;
+            searchSizeLong = (Double.parseDouble(args[2]))/2;
+        } catch (Exception e) {
+            searchSizeLat = 0.005;
+            searchSizeLong = 0.005;
+        }
+
         //Taking user input
         sc = new Scanner(System.in);
-        System.out.println("This program finds the highest elevation within 0.01 decimal degrees" +
-                "of a supplied point.");
-        System.out.println("Enter latitude: ");
+        System.out.println("This program finds the highest and lowest elevated points within 0.01 decimal degrees of a supplied point.");
+        System.out.print("Enter latitude in decimal degrees: ");
         latitude = sc.nextFloat();
 
-        if (latitude < -89.99 || latitude > 89.99) {
-            System.err.println("Invalid latitude. Must be between -89.99 and 89.99.");
+        //error checking
+        if (latitude < -90 + searchSizeLat || latitude > 90 - searchSizeLat) {
+            System.err.println("Invalid latitude. Must be between -90 and 90 including search bounds.");
             System.exit(1);
         }
 
-        System.out.println("Enter longitude: ");
+        System.out.print("Enter longitude in decimal degrees: ");
         longitude = sc.nextFloat();
-        if (longitude < -179.99 || longitude > 179.99) {
-            System.err.println("Invalid longitude. Must be between -179.99 and 179.99");
+
+        //error checking
+        if (longitude < -180 + searchSizeLong || longitude > 180 - searchSizeLong) {
+            System.err.println("Invalid longitude. Must be between -180 and 180 including search bounds.");
             System.exit(1);
         }
 
         //Populating coordinate arrays
         for (int i = 1; i < 256; i++) {
-            longitudes[256 + i] = longitude + i *  0.005 / 256;
-            latitudes[256 + i] = latitude + i * 0.005 / 256;
+            longitudes[256 + i] = longitude + i *  searchSizeLong / 256;
+            latitudes[256 + i] = latitude + i * searchSizeLat / 256;
         }
-
         for (int i = 0; i <= 256; i++) {
-            longitudes[256 - i] = longitude - i * 0.005 / 256;
-            latitudes[256 - i] = latitude - i * 0.005 / 256;
+            longitudes[256 - i] = longitude - i * searchSizeLong / 256;
+            latitudes[256 - i] = latitude - i * searchSizeLat / 256;
         }
-        //Single point google cloud API call
-        //URL url = new URL("https://maps.googleapis.com/maps/api/elevation/json?locations=" + latitude + "%2C" + longitude + "&key=" + apiKey);
 
         //Forming coordinate string for API call
         String coordinateString = "";
@@ -62,7 +70,7 @@ static double[] elevations = new double[512];
         //array API call
         URL url = new URL("https://maps.googleapis.com/maps/api/elevation/json?locations=" + coordinateString + "&key=" + apiKey);
 
-        //Processing API data returned
+        //Processing API data returned (this is messy but we're not good at regex :(
         String[] stringData;
         String json = new Scanner(url.openStream(), "UTF-8").useDelimiter("\\A").next();
         JsonParser jsonParser = new JsonParser();
@@ -93,11 +101,6 @@ static double[] elevations = new double[512];
             elevations[i] = Double.parseDouble(stringDataFixed[i]);
         }
 
-        //Filling array of gpsCoordinate objects, printing for diagnostics
-        for (int i = 0; i < 512; i++) {
-            coordinates[i] = new gpsCoordinate(latitudes[i], longitudes[i], elevations[i]);
-        }
-
         //finds maximum elevation
         double largest = -11035; //marianas trench
         double smallest = 8849; //mount everest
@@ -113,6 +116,7 @@ static double[] elevations = new double[512];
             }
         }
 
+        //returning data to user
         if (largest > -11035) {
             System.out.println("\nThe highest elevation within the region is: " + largest + " meters at location " + latitudes[indexL] + ", " + longitudes[indexL]);
         }
